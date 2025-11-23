ff854cf85d4991d3ca96f0c3516d2ba9d4460ef5
TechTronics <njbdowney@gmail.com>
2025-11-22 20:48:12 -0500

    Clarify display description and comments
    Updated comments and display description for clarity.

--- examples/Test_Display/Test_Display.ino
+++ examples/Test_Display/Test_Display.ino
@@ -1,7 +1,7 @@
 /*
   Test Display
 
-  This code will show a series of numbers, decimals, animations, and other functions. It works best on four digit displays.
+  This code will show a series of numbers, decimals, animations, and other functions on a seven segment display. It works best on four digit displays.
 
   The circuit:
   - Display pin A -> Arduino digital pin 5
@@ -41,50 +41,50 @@ int G = 11;
 int DP = 12;
 
 void setup() {
-display.begin(); // Initialize the display
-display.testDisplay(); // Activate all digits, segments, and decimals
-delay(1000); // Wait one second
-display.clearDisplay(); // Clear the display
+  display.begin(); // Initialize the display
+  display.testDisplay(); // Activate all digits, segments, and decimals
+  delay(1000); // Wait one second
+  display.clearDisplay(); // Clear the display
 }
 
 void loop() {
-display.showNumber(1234); // Display "1234"
-delay(1000); // Wait one second
-display.clearDisplay(); // Clear the display
-display.showNumber(2345); // Show the next number "2345"
-delay(100); // Wait one second
-display.clearDisplay(); // Clear the display...
-display.showNumber(3456);
-delay(100);
-display.clearDisplay();
-display.showNumber(4567);
-delay(100);
-display.clearDisplay();
-display.showNumber(5678);
-delay(100);
-display.clearDisplay();
-display.showNumber(6789);
-delay(100);
-display.clearDisplay();
-display.showNumber(7890);
-delay(1000);
-display.clearDisplay();
+  display.showNumber(1234); // Display "1234"
+  delay(1000); // Wait one second
+  display.clearDisplay(); // Clear the display
+  display.showNumber(2345); // Show the next number "2345"
+  delay(100); // Wait a tenth of a second
+  display.clearDisplay(); // Clear the display...
+  display.showNumber(3456);
+  delay(100);
+  display.clearDisplay();
+  display.showNumber(4567);
+  delay(100);
+  display.clearDisplay();
+  display.showNumber(5678);
+  delay(100);
+  display.clearDisplay();
+  display.showNumber(6789);
+  delay(100);
+  display.clearDisplay();
+  display.showNumber(7890);
+  delay(1000);
+  display.clearDisplay();
 
-delay(250); // Wait 0.25 seconds
+  delay(250); // Wait 0.25 seconds
 
-display.setDigit(1); // Select a digit
-display.showDecimal(); // Show the decimal
-delay(100); // Wait 0.1 seconds
-display.setDigit(2); // Move to the next digit
-display.showDecimal(); // Show this decimal
-delay(100); // Wait 0.1 seconds...
-display.setDigit(3);
-display.showDecimal();
-delay(100);
-display.setDigit(4);
-display.showDecimal();
-delay(1000);
-display.showNumber(1234); // Display the number "1234"
-delay(1000); // Wait one second
-display.clearDisplay(); // Clear the display
+  display.setDigit(1); // Select a digit
+  display.showDecimal(); // Show the decimal
+  delay(100); // Wait 0.1 seconds
+  display.setDigit(2); // Move to the next digit
+  display.showDecimal(); // Show this decimal
+  delay(100); // Wait 0.1 seconds...
+  display.setDigit(3);
+  display.showDecimal();
+  delay(100);
+  display.setDigit(4);
+  display.showDecimal();
+  delay(1000);
+  display.showNumber(1234); // Display the number "1234"
+  delay(1000); // Wait one second
+  display.clearDisplay(); // Clear the display
 }
